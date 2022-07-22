755ee789d7468f19156d5e0b6c355d08c2aee9d2
Chirag <chiraggoyal1910@gmail.com>
2022-07-22 14:06:34 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ credentials: true, origin: true }));
+
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => {
+    mongoose.connect(process.env.DATABASE_URL);
+});
